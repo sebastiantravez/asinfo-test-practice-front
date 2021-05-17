11eec37b69ef921b06c2b1b96df9415d168b4c9c
Sebastian <sebastiantravez0@gmail.com>
2021-05-16 20:45:25 -0500

    add variables to production eviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
   production: true
 };
+
+export const Business = {
+  idBusiness: '559cc947-719a-4d17-b4fa-398010ac6a15'
+}
+export const ambiente = {
+  urlServicioRest: 'http://localhost:8080/'
+}
